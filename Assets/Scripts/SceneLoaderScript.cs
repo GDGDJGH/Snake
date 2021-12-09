8f3d6405e9b277f4f10f75cb08b84e32b3fc3a3b
GDGDJGH <mizza2003@seznam.cz>
2021-12-09 08:55:08 +0100

    preventivní

---
+++ Assets/Scripts/SceneLoaderScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderScript : MonoBehaviour
+{
+    public void LoadNextScene(){
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadControll(){
+        SceneManager.LoadScene(3);
+    }
+
+    public void LoadMainMenu(){
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadNewGame(){
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
